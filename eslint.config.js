ea4e65f6ab9807f07eb88b02246828ca74c799c9
yuseoyeon <101778526+yuseoyeon@users.noreply.github.com>
2025-10-27 19:57:26 +0900

    chore: eslint, prettier 세팅

---
+++ eslint.config.js
@@ -0,0 +1,74 @@
+const { defineConfig, globalIgnores } = require('eslint/config');
+
+const globals = require('globals');
+
+const { fixupConfigRules } = require('@eslint/compat');
+
+const tsParser = require('@typescript-eslint/parser');
+const typescriptEslint = require('@typescript-eslint/eslint-plugin');
+const js = require('@eslint/js');
+
+const { FlatCompat } = require('@eslint/eslintrc');
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+module.exports = defineConfig([
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+
+    extends: fixupConfigRules(
+      compat.extends(
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:react-native/all',
+        'plugin:react-hooks/recommended',
+        'plugin:prettier/recommended',
+      ),
+    ),
+
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+
+    languageOptions: {
+      parser: tsParser,
+      ecmaVersion: 2021,
+      sourceType: 'module',
+      parserOptions: {},
+    },
+
+    plugins: {
+      '@typescript-eslint': typescriptEslint,
+    },
+
+    extends: compat.extends('plugin:@typescript-eslint/recommended'),
+
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      'react-native/no-inline-styles': 'off',
+      'react-native/no-color-literals': 'warn',
+      '@typescript-eslint/no-unused-vars': 'warn',
+
+      'prettier/prettier': [
+        'error',
+        {
+          endOfLine: 'auto',
+        },
+      ],
+    },
+  },
+  globalIgnores(['**/node_modules/', '**/dist/', '**/build/', '**/.expo/', '**/.expo-shared/']),
+]);
