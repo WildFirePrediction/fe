75b8fec645d658d9bc3ec26aedf13c37505745e7
yuseoyeon <101778526+yuseoyeon@users.noreply.github.com>
2025-11-02 23:15:42 +0900

    asset: svg 추가 - Location

--- assets/svgs/icons/index.ts
+++ assets/svgs/icons/index.ts
@@ -1,5 +1,7 @@
 import NavigationArrow from './NavigationArrow.svg';
 import Setting from './Setting.svg';
+import Location from './Location.svg';
 
 export { NavigationArrow };
 export { Setting };
+export { Location };
