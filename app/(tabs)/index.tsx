74a990da1671afdea841a8a54fadd17543365fa5
yuseoyeon <101778526+yuseoyeon@users.noreply.github.com>
2025-11-11 23:26:58 +0900

    feat: 메인화면 지도 구현

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,34 +1,85 @@
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import BottomSheet, { BottomSheetView } from '@gorhom/bottom-sheet';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { Button, SelectionButton } from '../../components';
+import * as Location from 'expo-location';
+import { Camera, NaverMapPolygonOverlay, NaverMapView } from '@mj-studio/react-native-naver-map';
+import theme from '../../styles/theme';
+import { RainIcon } from '../../assets/svgs/icons';
+import { coordsFire, coordsFirePredict } from '../../mock/fireAreaData';
+import { myRegionData } from '../../mock/myRegionsData';
 
 const WildFireMapScreen = () => {
-  const [selectedRegion, setSelectedRegion] = useState('상도동');
+  const [selectedRegion, setSelectedRegion] = useState(myRegionData.at(0));
+  const [camera, setCamera] = useState<Camera | undefined>(myRegionData.at(0));
 
   const bottomSheetRef = useRef<BottomSheet>(null);
   const handleSheetChanges = () => {};
 
-  const handleSelectRegion = (region: string) => {
+  const handleSelectRegion = (regionName: string) => {
+    const region = myRegionData.find(myRegion => myRegion.name === regionName);
     setSelectedRegion(region);
+    setCamera(region ? { ...region, zoom: 12 } : region);
   };
 
   const handleSetRegion = () => {};
 
+  useEffect(() => {
+    (async () => {
+      try {
+        const { granted } = await Location.requestForegroundPermissionsAsync();
+        if (granted) {
+          await Location.requestBackgroundPermissionsAsync();
+        }
+      } catch (e) {
+        console.error(`Location request has been failed: ${e}`);
+      }
+    })();
+  }, []);
+
   return (
     <GestureHandlerRootView>
       <SafeAreaView style={style.container}>
-        <BottomSheet style={style.bottomSheet} ref={bottomSheetRef} onChange={handleSheetChanges}>
+        <NaverMapView
+          style={{ flex: 1 }}
+          camera={camera}
+          isShowLocationButton={true}
+          locationOverlay={{
+            isVisible: true,
+          }}
+        >
+          <NaverMapPolygonOverlay
+            coords={coordsFire}
+            color={theme.color.mainTransparent}
+            outlineColor={theme.color.main}
+            outlineWidth={1}
+          />
+          <NaverMapPolygonOverlay
+            coords={coordsFirePredict}
+            color={theme.color.mainTransparent}
+            outlineColor={theme.color.main}
+            outlineWidth={1}
+          />
+        </NaverMapView>
+        <BottomSheet
+          style={style.bottomSheet}
+          ref={bottomSheetRef}
+          onChange={handleSheetChanges}
+          enablePanDownToClose={true}
+        >
           <BottomSheetView style={style.bottomSheetView}>
             <View style={style.bottomSheetRegionButtonsContainer}>
-              <SelectionButton selected={selectedRegion === '상도동'} onClick={handleSelectRegion}>
-                상도동
-              </SelectionButton>
-              <SelectionButton selected={selectedRegion === '흑석동'} onClick={handleSelectRegion}>
-                흑석동
-              </SelectionButton>
+              {myRegionData.map(myRegion => (
+                <SelectionButton
+                  key={myRegion.name}
+                  selected={selectedRegion?.name === myRegion.name}
+                  onClick={handleSelectRegion}
+                >
+                  {myRegion.name}
+                </SelectionButton>
+              ))}
               <Button
                 buttonType="setting"
                 onClick={handleSetRegion}
@@ -37,8 +88,18 @@ const WildFireMapScreen = () => {
                 지역 설정
               </Button>
             </View>
+            <View style={style.bottomSheetInformationContainer}>
+              <RainIcon style={style.bottomSheetInformationIconStyle} />
+              <View style={style.bottomSheetInformationTextContainer}>
+                <Text style={style.bottomSheetInformationTitleStyle}>호우 주의보</Text>
+                <Text style={style.bottomSheetInformationContentStyle}>동작구 흑석동</Text>
+              </View>
+            </View>
           </BottomSheetView>
         </BottomSheet>
+        <Button buttonType="floating" onClick={() => {}} customStyle={style.navigateButtonStyle}>
+          대피 안내
+        </Button>
       </SafeAreaView>
     </GestureHandlerRootView>
   );
@@ -58,6 +119,7 @@ const style = StyleSheet.create({
     paddingTop: 0,
     display: 'flex',
     flexDirection: 'column',
+    gap: 14,
   },
   bottomSheetRegionButtonsContainer: {
     width: '100%',
@@ -67,5 +129,34 @@ const style = StyleSheet.create({
   },
   bottomSheetRegionSettingButtonStyle: {
     marginLeft: 'auto',
+  },
+  bottomSheetInformationContainer: {
+    height: 100,
+    width: '100%',
+    padding: 26,
+    borderWidth: 1,
+    borderRadius: 20,
+    borderColor: theme.color.gray,
+    backgroundColor: theme.color.white,
+    flexDirection: 'row',
+    gap: 30,
+  },
+  bottomSheetInformationIconStyle: {
+    alignSelf: 'center',
+  },
+  bottomSheetInformationTextContainer: {
+    display: 'flex',
+  },
+  bottomSheetInformationTitleStyle: {
+    fontSize: 20,
+  },
+  bottomSheetInformationContentStyle: {
+    fontSize: 16,
+  },
+  navigateButtonStyle: {
+    position: 'absolute',
+    bottom: 220,
+    right: 10,
+    alignSelf: 'flex-end',
   },
 });
---
+++ mock/fireAreaData.tsx
@@ -0,0 +1,13 @@
+export const coordsFire = [
+  { latitude: 37.5, longitude: 126.9 },
+  { latitude: 37.55, longitude: 126.95 },
+  { latitude: 37.52, longitude: 127 },
+  { latitude: 37.48, longitude: 126.98 },
+];
+
+export const coordsFirePredict = [
+  { latitude: 37.5, longitude: 126.91 },
+  { latitude: 37.55, longitude: 126.96 },
+  { latitude: 37.52, longitude: 127.1 },
+  { latitude: 37.48, longitude: 127 },
+];
