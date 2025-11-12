5ad21320b4bf2aea1e3df828374fb753f5d5b780
yuseoyeon <101778526+yuseoyeon@users.noreply.github.com>
2025-11-12 23:24:16 +0900

    asset: 아이콘 추가

--- assets/svgs/icons/index.ts
+++ assets/svgs/icons/index.ts
@@ -2,8 +2,12 @@ import NavigationArrow from './NavigationArrow.svg';
 import Setting from './Setting.svg';
 import Location from './Location.svg';
 import Rain from './Rain.svg';
+import Cancel from './Cancel.svg';
+import RegionItemDelete from './RegionItemDelete.svg';
 
 export { NavigationArrow };
 export { Setting as SettingIcon };
 export { Location as LocationIcon };
 export { Rain as RainIcon };
+export { Cancel as CancelIcon };
+export { RegionItemDelete as RegionItemDeleteIcon };
