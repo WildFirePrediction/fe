541348353b9f4656f167bd3afc9870e0291a6e2e
yuseoyeon <101778526+yuseoyeon@users.noreply.github.com>
2025-11-26 03:15:06 +0900

    refactor: 대피 경로 미리보기 화면 수정

--- app/(evacuation)/routePreview/[slug].tsx
+++ app/(evacuation)/routePreview/[slug].tsx
@@ -3,6 +3,7 @@ import {
   NaverMapPathOverlay,
   NaverMapPolygonOverlay,
   NaverMapView,
+  NaverMapViewRef,
 } from '@mj-studio/react-native-naver-map';
 import { useLocalSearchParams, useRouter } from 'expo-router';
 import { StyleSheet, View } from 'react-native';
@@ -10,16 +11,19 @@ import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { CancelIcon } from '../../../assets/svgs/icons';
 import { Button } from '../../../components';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { evacuationRouteData } from '../../../mock/evacuationRouteData';
 import theme from '../../../styles/theme';
 import { coordsFire, coordsFirePredict } from '../../../mock/fireAreaData';
+import * as Location from 'expo-location';
 
 const EvacuationRoutePreview = () => {
   const router = useRouter();
   const id = useLocalSearchParams<{ slug: string }>();
+  const mapRef = useRef<NaverMapViewRef>(null);
+
   const [myLocation, setMyLocation] = useState({
-    latitude: 37.505278,
+    latitude: 40,
     longitude: 126.954613,
     zoom: 15,
   });
@@ -30,6 +34,16 @@ const EvacuationRoutePreview = () => {
 
   const handleEvacuation = () => {
     router.replace(`/(evacuation)/route/${id}`);
+  };
+
+  const setCurrentPosition = async () => {
+    const position = await Location.getCurrentPositionAsync();
+    setMyLocation({
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude,
+      zoom: 15,
+    });
+    console.log({ latitude: position.coords.latitude, longitude: position.coords.longitude });
   };
 
   useEffect(() => {
@@ -49,11 +63,22 @@ const EvacuationRoutePreview = () => {
     setMyLocation(prev => ({ ...prev, bearing: bearing }));
   }, [myLocation.latitude, myLocation.longitude]);
 
+  useEffect(() => {
+    mapRef.current?.setLocationTrackingMode('NoFollow');
+    setCurrentPosition();
+  }, []);
+
   return (
     <GestureHandlerRootView>
       <SafeAreaView style={style.screen}>
         <View style={style.container}>
-          <NaverMapView style={style.naverMap} camera={myLocation} isShowCompass={false}>
+          <NaverMapView
+            ref={mapRef}
+            style={style.naverMap}
+            camera={myLocation}
+            isShowCompass={false}
+            isShowLocationButton={true}
+          >
             <NaverMapPolygonOverlay
               coords={coordsFire}
               color={theme.color.mainTransparent}
@@ -66,10 +91,17 @@ const EvacuationRoutePreview = () => {
               outlineColor={theme.color.main}
               outlineWidth={1}
             />
-            <NaverMapPathOverlay coords={evacuationRouteData} width={12} color={theme.color.main} />
+            <NaverMapPathOverlay
+              coords={evacuationRouteData}
+              width={12}
+              color={theme.color.rain}
+              outlineWidth={2}
+              outlineColor={theme.color.white}
+            />
             <NaverMapMarkerOverlay
               latitude={myLocation.latitude}
               longitude={myLocation.longitude}
+              image={require('../../../assets/pngs/departureMarker.png')}
             />
             {evacuationRouteData.at(-1) !== undefined && (
               <NaverMapMarkerOverlay
@@ -79,6 +111,7 @@ const EvacuationRoutePreview = () => {
                   text: '중앙대학교병원중앙관 지하주차장 1~3층 대피소',
                   requestedWidth: 30,
                 }}
+                image={require('../../../assets/pngs/arriveMarker.png')}
               />
             )}
           </NaverMapView>
