179eab45221ff6f77e4c2f148ffed1cd429449ed
yuseoyeon <101778526+yuseoyeon@users.noreply.github.com>
2025-12-06 23:45:37 +0900

    refactor: 바텀탭 코드 리팩토링

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -4,6 +4,12 @@ import theme from '../../styles/theme';
 import { StyleSheet, Text } from 'react-native';
 
 const TabsLayout = () => {
+  const tabScreens = [
+    { name: 'index', title: '산불지도', Icon: FireIcon },
+    { name: 'disasterInfo', title: '재난 정보', Icon: InformationIcon },
+    { name: 'myPage', title: '마이페이지', Icon: MyPageIcon },
+  ];
+
   return (
     <Tabs
       screenOptions={{
@@ -12,60 +18,27 @@ const TabsLayout = () => {
         tabBarStyle: style.tabBar,
       }}
     >
-      <Tabs.Screen
-        name="index"
-        options={{
-          headerShown: false,
-          title: '산불지도',
-          tabBarIcon: ({ focused }) => (
-            <FireIcon
-              color={focused ? theme.color.black : theme.color.darkGray1}
-              style={style.icon}
-              width={28}
-              height={28}
-            />
-          ),
-          tabBarLabel: ({ color }) => (
-            <Text style={{ color: color, ...style.label }}>산불지도</Text>
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="disasterInfo"
-        options={{
-          headerShown: false,
-          title: '재난 정보',
-          tabBarIcon: ({ focused }) => (
-            <InformationIcon
-              color={focused ? theme.color.black : theme.color.darkGray1}
-              style={style.icon}
-              width={28}
-              height={28}
-            />
-          ),
-          tabBarLabel: ({ color }) => (
-            <Text style={{ color: color, ...style.label }}>재난 정보</Text>
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name="myPage"
-        options={{
-          headerShown: false,
-          title: '마이페이지',
-          tabBarIcon: ({ focused }) => (
-            <MyPageIcon
-              color={focused ? theme.color.black : theme.color.darkGray1}
-              style={style.icon}
-              width={28}
-              height={28}
-            />
-          ),
-          tabBarLabel: ({ color }) => (
-            <Text style={{ color: color, ...style.label }}>마이페이지</Text>
-          ),
-        }}
-      />
+      {tabScreens.map(({ name, title, Icon }) => (
+        <Tabs.Screen
+          key={name}
+          name={name}
+          options={{
+            headerShown: false,
+            title: title,
+            tabBarIcon: ({ focused }) => (
+              <Icon
+                color={focused ? theme.color.black : theme.color.darkGray1}
+                style={style.icon}
+                width={28}
+                height={28}
+              />
+            ),
+            tabBarLabel: ({ color }) => (
+              <Text style={{ color: color, ...style.label }}>{title}</Text>
+            ),
+          }}
+        />
+      ))}
     </Tabs>
   );
 };
