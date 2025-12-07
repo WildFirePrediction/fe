7dd99976223f09cc65b799443b71aa8452800f58
yuseoyeon <101778526+yuseoyeon@users.noreply.github.com>
2025-12-07 18:37:22 +0900

    feat: 대피 목적지 전역 상태 관리

---
+++ context/destinationContext.tsx
@@ -0,0 +1,30 @@
+import React, { createContext, useContext, useState } from 'react';
+import { FullCoordWithName } from '../types/locationCoord';
+
+interface DestinationContextType {
+  destination: FullCoordWithName | undefined;
+  setDestination: React.Dispatch<React.SetStateAction<FullCoordWithName | undefined>>;
+}
+
+interface ProviderProps {
+  children: React.ReactNode;
+}
+
+const DestinationContext = createContext<DestinationContextType | null>(null);
+
+export const useDestination = () => {
+  const context = useContext(DestinationContext);
+  if (!context) {
+    throw new Error('context 오류');
+  }
+  return context;
+};
+
+export const DestinationProvider: React.FC<ProviderProps> = ({ children }: ProviderProps) => {
+  const [destination, setDestination] = useState<FullCoordWithName | undefined>();
+  const value = { destination, setDestination };
+
+  return <DestinationContext.Provider value={value}>{children}</DestinationContext.Provider>;
+};
+
+export default DestinationContext;
