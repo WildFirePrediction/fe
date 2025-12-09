d2dfc8796b2411a3a1022c31d4cded1c15eaa28e
yuseoyeon <101778526+yuseoyeon@users.noreply.github.com>
2025-12-09 21:15:45 +0900

    feat: 경로 미리보기 화면 거리&시간 정보 추가

--- app/(evacuation)/routePreview/[slug].tsx
+++ app/(evacuation)/routePreview/[slug].tsx
@@ -6,7 +6,7 @@ import {
   NaverMapViewRef,
 } from '@mj-studio/react-native-naver-map';
 import { useLocalSearchParams, useRouter } from 'expo-router';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { CancelIcon } from '../../../assets/svgs/icons';
@@ -29,6 +29,8 @@ const EvacuationRoutePreview = () => {
 
   const [startLocation, setStartLocation] = useState<Camera | undefined>();
   const [route, setRoute] = useState<FullCoord[] | null>(null);
+  const [distance, setDistance] = useState(0);
+  const [time, setTime] = useState(0);
 
   const postRoute = usePostRoutes();
 
@@ -64,13 +66,16 @@ const EvacuationRoutePreview = () => {
       },
       {
         onSuccess: response => {
-          if (response.result)
+          if (response.result) {
+            setDistance(response.result.totalDistance);
+            setTime(response.result.totalTime);
             setRoute(
               response.result.path.map(coord => ({
                 latitude: coord[1],
                 longitude: coord[0],
               })),
             );
+          }
         },
       },
     );
@@ -125,6 +130,11 @@ const EvacuationRoutePreview = () => {
               />
             )}
           </NaverMapView>
+          <View style={style.routeInfoContainer}>
+            <Text style={style.routeInfoDistanceText}>{distance}m</Text>
+            <View style={style.routeInfoLine} />
+            <Text style={style.routeInfoTimeText}>{time}분</Text>
+          </View>
           <CancelIcon style={style.closeIcon} onPress={handleGoBack} />
           <View style={style.buttonContainer}>
             <Button buttonType="full" onClick={handleEvacuation}>
@@ -164,5 +174,44 @@ const style = StyleSheet.create({
     bottom: 20,
     start: 20,
     end: 20,
+    shadowColor: theme.color.black,
+    shadowOpacity: 0.3,
+    shadowOffset: { width: 0, height: 4 },
+    zIndex: 10,
+  },
+  alertText: {
+    fontSize: 20,
+    color: theme.color.white,
+    fontWeight: 'bold',
+  },
+  routeInfoContainer: {
+    position: 'absolute',
+    flexDirection: 'row',
+    alignSelf: 'flex-end',
+    alignItems: 'center',
+    paddingVertical: 16,
+    paddingHorizontal: 20,
+    gap: 10,
+    borderRadius: 30,
+    borderWidth: 1,
+    borderColor: theme.color.gray,
+    backgroundColor: theme.color.white,
+    shadowColor: theme.color.black,
+    shadowOpacity: 0.3,
+    shadowOffset: { width: 0, height: 4 },
+    marginTop: 20,
+    marginEnd: 10,
+  },
+  routeInfoDistanceText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  routeInfoLine: {
+    backgroundColor: theme.color.gray,
+    width: 1,
+  },
+  routeInfoTimeText: {
+    fontSize: 16,
+    color: theme.color.darkGray2,
   },
 });
